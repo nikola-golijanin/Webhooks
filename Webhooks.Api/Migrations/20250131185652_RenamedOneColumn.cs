69ef062a2e4dd549bbdae3a6a2daab6dda81390b
nikola-golijanin <golijanin.nikola@outlook.com>
2025-01-31 19:57:51 +0100

    Rename column 'ReponseStatusCode' to 'ResponseStatusCode' in delivery_attempts table

---
+++ Webhooks.Api/Migrations/20250131185652_RenamedOneColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webhooks.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamedOneColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReponseStatusCode",
+                schema: "webhooks",
+                table: "delivery_attempts",
+                newName: "ResponseStatusCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ResponseStatusCode",
+                schema: "webhooks",
+                table: "delivery_attempts",
+                newName: "ReponseStatusCode");
+        }
+    }
+}
