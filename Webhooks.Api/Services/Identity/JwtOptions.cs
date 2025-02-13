d5db920d2c4ed926ed065640c811ea22f71a386e
nikola-golijanin <golijanin.nikola@outlook.com>
2025-02-13 12:25:23 +0100

    Added JWT Options

---
+++ Webhooks.Api/Services/Identity/JwtOptions.cs
@@ -0,0 +1,53 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Webhooks.Api.Services.Identity;
+
+public class JwtOptions
+{
+    public string Issuer { get; init; }
+    public string Audience { get; init; }
+    public string SecretKey { get; init; } 
+}
+
+public class JwtOptionsSetup : IConfigureOptions<JwtOptions>
+{
+    private const string SectionName = "Jwt";
+    private readonly IConfiguration _configuration;
+
+    public JwtOptionsSetup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Configure(JwtOptions options)
+    {
+        _configuration.GetSection(SectionName).Bind(options);
+    }
+}
+
+public class JwtBearerOptionsSetup : IPostConfigureOptions<JwtBearerOptions>
+{
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtBearerOptionsSetup(IOptions<JwtOptions> jwtOptions)
+    {
+        _jwtOptions = jwtOptions.Value;
+    }
+    
+    public void PostConfigure(string? name, JwtBearerOptions options)
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = _jwtOptions.Issuer,
+            ValidAudience = _jwtOptions.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey))
+        };
+    }
+}
