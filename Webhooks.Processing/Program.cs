73febefaeb4f34c7485d9e94bb6bd9875d22ab2a
nikola-golijanin <golijanin.nikola@outlook.com>
2025-02-17 11:29:20 +0100

    Add Webhooks.Processing project with Docker support and RabbitMQ integration

---
+++ Webhooks.Processing/Program.cs
@@ -0,0 +1,45 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Webhooks.Infrastructure.Webhooks;
+using Webhooks.Persistance;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddHttpClient();
+builder.Services.AddDbContext<WebhooksDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
+
+builder.Services.AddMassTransit(busConfig =>
+        {
+            busConfig.SetKebabCaseEndpointNameFormatter();
+
+            busConfig.AddConsumer<WebhookDispatchedConsumer>();
+            busConfig.AddConsumer<WebhookTriggeredConsumer>();
+
+            busConfig.UsingRabbitMq((context, config) =>
+            {
+                config.Host(new Uri(builder.Configuration["RabbitMQ:Host"]!), host =>
+                {
+                    host.Username(builder.Configuration["RabbitMQ:Username"]!);
+                    host.Password(builder.Configuration["RabbitMQ:Password"]!);
+                });
+
+                config.ConfigureEndpoints(context);
+            });
+        });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
