9822cdb889ba72084525b2496f9f44a1924e3901
Nikola Golijanin <64159013+nikola-golijanin@users.noreply.github.com>
2025-02-25 11:18:57 +0100

    added todo comment

--- Webhooks.Application/Users/UserService.cs
+++ Webhooks.Application/Users/UserService.cs
@@ -40,7 +40,9 @@ public class UserService : IUserService
 
         var defaultProfile = await _context.Set<Profile>()
             .FirstOrDefaultAsync(p => p.Name == "Subscriber", cancellationToken);
-
+        
+        //TODO add ArgumentNullException.ThrowIfNull(defaultProfile); instead of this check
+        //  because "Subscriber" profile must be in db
         if (defaultProfile is null)
             return Result.Failure(DomainErrors.Profile.NoProfilesFound);
 
