0673dc3c2ca0f92d9b0d2baf6e499465dec10887
nikola-golijanin <golijanin.nikola@outlook.com>
2025-11-03 13:48:04 +0100

    Docker and docker compose files

--- Webhooks.API/Program.cs
+++ Webhooks.API/Program.cs
@@ -43,11 +43,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
     await app.ApplyMigrationsAsync();
 }
+
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
